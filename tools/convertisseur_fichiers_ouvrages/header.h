7fc986909a4d218204a5918a32e95e28484b7a53
Lucas Embrechts <lucas.embrechts@gmail.com>
2023-03-11 11:59:17 +0100

    wip

---
+++ tools/convertisseur_fichiers_ouvrages/header.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <string.h>
+
+typedef struct livre {
+    char code[11];
+    char titre[100];
+    char auteur[100];
+    int date;
+    char editeur[50];
+}Livre;
